395dcdb8bf2104c9d6ee8d46bb158a1530e75525
JasonZheng20 <jzzheng@stanford.edu>
2018-05-23 13:17:59 -0700

    added more factors into k-means, but they are relatively unweighted and unoptimized and do not discount missing or uninitialized values

--- h5reader.py
+++ h5reader.py
@@ -41,6 +41,9 @@ def constructCentroid(avgFieldsDict):
         newCentroid[field] = avgFieldsDict[field]
     return newCentroid
 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
 #-------------------------------------------------------------------------------
 #distanceSongs(song1, song2)
 
@@ -51,7 +54,35 @@ def constructCentroid(avgFieldsDict):
 #-------------------------------------------------------------------------------
 def distanceSongs(song1, song2):
     #TODO DO A CHECK TO SEE IF YEAR (AND OTHER DATA FIELDS ARE UNINIT (0))
-    return abs(song1.year - song2.year)
+    #if a data field is not provided, don't add it to the distance, but we still need to normalize so a blank song isn't closer than a song thats missing some fields
+    #TODO: THIS ASSUMES NO WEIGHTING AND DOESNT CHECK FOR BLANK FIELD
+            # self.duration = 0 #assume always provided
+            # self.year = 0 #If it is not provided, it is 0
+            # self.key = 0 #Same deal as mode, see below, though key can take on more values
+            # self.keyConfidence = 0 #if 0.0 do not use self.key
+            # self.generalLoudness = 0 #assume it is always given
+            # self.mode = 0 #appears to always be 0 or 1, don't include if modeConfidence is 0.0
+            # self.modeConfidence = 0 #if modeConfidence is 0.0, assume don't use mode info, even if provided TODO: experiment with using despite 0.0
+            # self.tempo = 0 #if not provided, it will be 0
+            # self.timeSigniature = 0 #if not provided it will be 0
+            # self.timeSigniatureConfidence = 0 #if this is 0.0 do not use time sig
+        # distance = 2000*sigmoid(song1.year - song2.year)
+        # distance += 3*sigmoid(song1.duration - song2.duration)
+        # distance += 1.2*sigmoid(song1.key - song2.key)
+        # distance += 4*sigmoid(song1.generalLoudness - song2.generalLoudness)
+        # distance += 1.2*sigmoid(song1.mode - song2.mode)
+        # distance += 4*sigmoid(song1.tempo - song2.tempo)
+        # distance += sigmoid(song1.timeSigniature - song2.timeSigniature)
+        #Using sigmoid fails to cluster them
+    #TODO TRAIN COEFFICIENTS THAT GIVE US THE MOST CONSISTENT CLUSTERINGS ACROSS K-MEANS RUNS (AKA for each clustering, get closest clustering in other trial, and minimize distance)
+    distance = 1.2*abs(song1.year - song2.year)
+    distance += 2*abs(song1.duration - song2.duration)
+    distance += abs(song1.key - song2.key)
+    distance += 3*abs(song1.generalLoudness - song2.generalLoudness)
+    distance += abs(song1.mode - song2.mode)
+    distance += 3*abs(song1.tempo - song2.tempo)
+    distance += abs(song1.timeSigniature - song2.timeSigniature)
+    return distance
 
 #-------------------------------------------------------------------------------
 #kMeansAllSongs(songsDict, numCentroids)
@@ -63,7 +94,7 @@ def distanceSongs(song1, song2):
 
 #TODO: update kMeans so that it works on an arbitrary number of fields, as described above
 #-------------------------------------------------------------------------------
-def kMeansAllSongs(songsDict, numCentroids = 5, T = 100000):
+def kMeansAllSongs(songsDict, numCentroids = 5, T = 1000):
     centroids = randomCentroids(songsDict, numCentroids)
     for i in xrange(0, T):
         assignments = [[] for j in range(len(centroids))] #each subarray is a cluster
@@ -80,21 +111,58 @@ def kMeansAllSongs(songsDict, numCentroids = 5, T = 100000):
         new_centroids = [CONST_FILLER_SONG for j in range(len(centroids))]
         totals = [len(assignments[j]) for j in xrange(0, len(centroids))] #TODO THIS SECTION ONLY ASSUMES YEAR --------
     	for j in xrange(0, len(centroids)): #look at a centroid (cluster)
-            if len(assignments[j]) == 0: continue
+            thisCluster = assignments[j]
+            if len(thisCluster) == 0: continue
+
+
             #------------------------------------------------------------------------------------
             #TODO: Generalize the averaging to multiple fields, check for uninitialized values (see Song class for details)
             #------------------------------------------------------------------------------------
             clusterTotalYears = 0.
+            clusterTotalDuration = 0.
+            clusterTotalKeys = 0.
+            #use keyConfidence somehow for weighting in distance function
+            clusterTotalLoudness = 0.
+            clusterTotalMode = 0.
+            #use modeConfidence
+            clusterTotalTempo = 0.
+            clusterTotalTimeSig = 0.
+            #use confidence
+            #TODO: NESTED FOR LOOP INSTEAD, CHECK IF FIELD IS GIVEN BEFORE APPEND
             for k in xrange(0, totals[j]): #for each assignment in that cluster
-                clusterTotalYears += assignments[j][k].year
+                #only do this if the field is given, for each field
+                thisSong = thisCluster[k]
+                clusterTotalYears += thisSong.year
+                clusterTotalDuration += thisSong.duration
+                clusterTotalKeys += thisSong.key
+                clusterTotalLoudness += thisSong.generalLoudness
+                clusterTotalMode += thisSong.mode
+                clusterTotalTempo += thisSong.tempo
+                clusterTotalTimeSig += thisSong.timeSigniature
+            #TODO: INSTEAD OF TOTAL[j], it should be number of elements where the specific field is given
             avgYear = clusterTotalYears / totals[j]
-            #I have implemented a constructCentroid function here that takes in a dict of values
+            avgDuration = clusterTotalDuration / totals[j]
+            avgKey = clusterTotalKeys / totals[j]
+            avgLoudness = clusterTotalLoudness / totals[j]
+            avgMode = clusterTotalMode / totals[j]
+            avgTempo = clusterTotalTempo / totals[j]
+            avgTimeSig = clusterTotalTimeSig / totals[j]
+            #TODO: I have implemented a constructCentroid function here that takes in a dict of values, use it
             #------------------------------------------------------------------------------------
             newCentroid = Song('nan')
             newCentroid.year = avgYear
+            newCentroid.duration = avgDuration
+            newCentroid.key = avgKey
+            newCentroid.generalLoudness = avgLoudness
+            newCentroid.mode = avgMode
+            newCentroid.tempo = avgTempo
+            newCentroid.timeSigniature = avgTimeSig
             #------------------------------------------------------------------------------------
+
+
             new_centroids[j] = newCentroid
         if new_centroids == centroids:
+            print "CONVERGENCE AFTER " + str(i) + " TRIALS"
             break
         centroids = new_centroids
     return centroids, assignments
@@ -141,7 +209,7 @@ class Song:
         self.similarArtists = []
         self.terms = []
 
-        #Numerical Fields --TODO: CHECK WHAT DEFAULTS ARE
+        #Numerical Fields
         self.duration = 0 #assume always provided
         self.year = 0 #If it is not provided, it is 0
         self.key = 0 #Same deal as mode, see below, though key can take on more values
@@ -204,7 +272,8 @@ class Song:
 #------------------------------------------------------------------------------------
 #TODO: Update this so equivalence requires all fields are equal
 #------------------------------------------------------------------------------------
-        return self.year == other.year
+#DOESNT USE CONFIDENCES RN
+        return self.year == other.year and self.duration == other.duration and self.key == other.key and self.generalLoudness == other.generalLoudness and self.mode == other.mode and self.tempo == other.tempo and self.timeSigniature == other.timeSigniature
 
     def __ne__(self,other):
         return self.year != other.year
@@ -272,9 +341,15 @@ CONST_FILLER_SONG.year = float('inf')
 print "-----------------------Loading Song Data from Pickle------------------------"
 newDict = load("songsDict")
 print "--------------------------Data Loading is Complete--------------------------"
-centroids, assignments = kMeansAllSongs(newDict, 5) #the centroids are not always the same for year
+centroids, assignments = kMeansAllSongs(newDict, 5, 1000) #the centroids are not always the same for year
 # #each centroid is a Song object. Each element in assignments is an array of Song objects
 # #might need to change this so that trackid is readily accessible
 #
-# for centroid in centroids: #To see what the years are for the centroids
+for subArr in assignments:
+    print len(subArr)
+i=0
+# for centroid in centroids: #To see what the centroids are
+#     print "CENTROID #" + str(i)
+#     centroid.printSong()
+#     i += 1
 #     print centroid.year
