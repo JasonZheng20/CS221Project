d69ac6b1f73f645c17758acef84f24c9f712634e
JasonZheng20 <jzzheng@stanford.edu>
2018-05-22 20:44:55 -0700

    added more numerical fields, and provided TODO comments

--- h5reader.py
+++ h5reader.py
@@ -15,11 +15,11 @@ import random
 #-------------------------------------------------------------------------------
 # favoriteSong(cluster)
 
-#gets the lowest distance song to a centroid
+#gets the lowest distance song to a centroid (fun function)
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-# randomCentroids(songsDict, numCentroids)
+#randomCentroids(songsDict, numCentroids)
 
 #Takes songsDict, and constructs numCentroids # of random centroids
 #-------------------------------------------------------------------------------
@@ -30,22 +30,24 @@ def randomCentroids(songsDict, numCentroids):
     return centroids
 
 #-------------------------------------------------------------------------------
+#constructCentroid()
+
+#Takes a dictionary indicating the average of all of the fields of all songs
+#in a cluster, and generates a new centroid Song containing that information
+#-------------------------------------------------------------------------------
+def constructCentroid(avgFieldsDict):
+    newCentroid = Song('centroid')
+    for field in avgFieldsDict:
+        newCentroid[field] = avgFieldsDict[field]
+    return newCentroid
+
+#-------------------------------------------------------------------------------
 #distanceSongs(song1, song2)
 
 #Takes two song objects and computes their distance
 #Currently computes distance solely by year
-#Distance metrics to add:
-#
-#1. Artist similarity
-#2. Same Album ( learn weights for that? )
-#3. Duration of song
-#4. Genre of song
-#5. terms (a bunch of strings, probably booleans again)
-
-#Extended metrics:
-#6. Max volume
-#7. Pitches (?)
-#8. Research dataset more
+#TODO: Create an algorithm to be able to weight all of the terms properly to get accurate distances
+#TODO: We don't really have labeled data for song distance, so it will have to be unsupervised(?)
 #-------------------------------------------------------------------------------
 def distanceSongs(song1, song2):
     #TODO DO A CHECK TO SEE IF YEAR (AND OTHER DATA FIELDS ARE UNINIT (0))
@@ -78,15 +80,21 @@ def kMeansAllSongs(songsDict, numCentroids = 5, T = 100000):
         new_centroids = [CONST_FILLER_SONG for j in range(len(centroids))]
         totals = [len(assignments[j]) for j in xrange(0, len(centroids))] #TODO THIS SECTION ONLY ASSUMES YEAR --------
     	for j in xrange(0, len(centroids)): #look at a centroid (cluster)
-            if len(assignments[j]) == 0: continue #TODO THIS SHOULD POSSIBLY CHANGE?
+            if len(assignments[j]) == 0: continue
+            #------------------------------------------------------------------------------------
+            #TODO: Generalize the averaging to multiple fields, check for uninitialized values (see Song class for details)
+            #------------------------------------------------------------------------------------
             clusterTotalYears = 0.
             for k in xrange(0, totals[j]): #for each assignment in that cluster
                 clusterTotalYears += assignments[j][k].year
             avgYear = clusterTotalYears / totals[j]
+            #I have implemented a constructCentroid function here that takes in a dict of values
+            #------------------------------------------------------------------------------------
             newCentroid = Song('nan')
-            newCentroid.year = avgYear #TODO SHOULD I DO INT? OR NA
+            newCentroid.year = avgYear
+            #------------------------------------------------------------------------------------
             new_centroids[j] = newCentroid
-        if new_centroids == centroids: #uh oh this might not work since im comparing objects with instances
+        if new_centroids == centroids:
             break
         centroids = new_centroids
     return centroids, assignments
@@ -122,24 +130,28 @@ def getTrackidFromTrackName(name):
 #class Song
 
 #Contains relevant information about a song, maybe represent with sparse vectors
-#TODO So far just contains metadata, not audio data
 #-------------------------------------------------------------------------------
 class Song:
     def __init__(self, trackid):
         self.trackid = trackid
         self.album = "n/a"
         self.title = "n/a"
-        self.artistid = 0
+        self.artistid = "nan" #If not provided, it is 'nan'
         self.artistName = "n/a"
         self.similarArtists = []
-        self.duration = 0
-        self.year = 0 #<=============currently being used
-        # self.terms = []
+        self.terms = []
 
-        # self.maxLoudness = 0
-        # self.maxLoudnessDuration = 0
-        # self.segmentPitches = 0
-        #Not sure what other fields to use/how to handle them
+        #Numerical Fields --TODO: CHECK WHAT DEFAULTS ARE
+        self.duration = 0 #assume always provided
+        self.year = 0 #If it is not provided, it is 0
+        self.key = 0 #Same deal as mode, see below, though key can take on more values
+        self.keyConfidence = 0 #if 0.0 do not use self.key
+        self.generalLoudness = 0 #assume it is always given
+        self.mode = 0 #appears to always be 0 or 1, don't include if modeConfidence is 0.0
+        self.modeConfidence = 0 #if modeConfidence is 0.0, assume don't use mode info, even if provided TODO: experiment with using despite 0.0
+        self.tempo = 0 #if not provided, it will be 0
+        self.timeSigniature = 0 #if not provided it will be 0
+        self.timeSigniatureConfidence = 0 #if this is 0.0 do not use time sig
 
     #For debugging purposes
     def printSong(self):
@@ -152,26 +164,46 @@ class Song:
         print "SIMILAR_ARTISTS: "  + str(self.similarArtists)
         print "DURATION: "  + str(self.duration)
         print "YEAR: "  + str(self.year)
-        # print "TERMS: "  + str(self.terms)
+        print "TERMS: "  + str(self.terms)
+        print "KEY: " + str(self.key)
+        print "KEY_CONFIDENCE: " + str(self.keyConfidence)
+        print "LOUDNESS: " + str(self.generalLoudness)
+        print "MODE: " + str(self.mode)
+        print "MODE_CONFIDENCE: " + str(self.modeConfidence)
+        print "TEMPO: " + str(self.tempo)
+        print "TIME_SIG: " + str(self.timeSigniature)
+        print "TIME_SIG_CONFIDENCE: " + str(self.timeSigniatureConfidence)
         print "-----------------------------------------"
 
-    def populateFields(self): #Currently only contains the most basic metadata
+    def populateFields(self):
         filename = './data/'+ self.trackid[2] + '/' + self.trackid[3] + '/' + self.trackid[4] + '/' + self.trackid + ".h5"
-        f = h5py.File(filename, 'r') #introduce something to throw on failure
+        f = h5py.File(filename, 'r')
         metadata = f['metadata']
         songMetaData = metadata['songs'].value[0]
+        songsMeta = f['analysis']['songs'].value[0]
         self.album = songMetaData[14]
         self.title = songMetaData[18]
         self.artistid = songMetaData[5]
         self.artistName = songMetaData[9]
         self.similarArtists = np.asarray(metadata['similar_artists'].value[0])
-        # self.duration =
+        self.duration = songsMeta[3]
         self.year = f['musicbrainz']['songs'].value[0][1]
-        # self.terms = metadata['artist_terms'].value[0] #add weight and frequency potentially
-
-        #TODO: MUSIC FEATURES
+        self.key = songsMeta[21]
+        self.keyConfidence = songsMeta[22] #WHAT TO DO HERE
+        self.generalLoudness = songsMeta[23]
+        self.mode = songsMeta[24]
+        self.modeConfidence = songsMeta[25] #WHAT TO DO HERE
+        self.tempo = songsMeta[27]
+        self.timeSigniature = songsMeta[28]
+        self.timeSigniatureConfidence = songsMeta[29] #WHAT TO DO HERE
+        self.terms = metadata['artist_terms'].value #add weight and frequency potentially
+        #TODO: weight for terms
+        #TODO: frequency for terms
 
     def __eq__(self, other): #TODO: FOR NOW ONLY CHECKS YEAR EQUIVALENCE
+#------------------------------------------------------------------------------------
+#TODO: Update this so equivalence requires all fields are equal
+#------------------------------------------------------------------------------------
         return self.year == other.year
 
     def __ne__(self,other):
@@ -190,6 +222,7 @@ def populateSongs(songList):
         i += 1
         obj = Song(song)
         obj.populateFields()
+        # obj.printSong() #Take a look at this if you would like to further scrutinize extreme values
         songObjectArr.update({song:obj})
         sys.stdout.write("\r%d/10000 Songs Read" % i)
         sys.stdout.flush()
@@ -232,7 +265,7 @@ def readAndSavePickle(inputPath):
 #-------------------------------------------------------------------------------
 #Live Scripts to actually do stuff:
 #-------------------------------------------------------------------------------
-# readAndSavePickle('./AdditionalFiles/subset_unique_tracks.txt')
+# readAndSavePickle('./AdditionalFiles/subset_unique_tracks.txt') #(~1 min 50 seconds)
 CONST_FILLER_SONG = Song('filler')
 CONST_FILLER_SONG.year = float('inf')
 
@@ -240,8 +273,8 @@ print "-----------------------Loading Song Data from Pickle---------------------
 newDict = load("songsDict")
 print "--------------------------Data Loading is Complete--------------------------"
 centroids, assignments = kMeansAllSongs(newDict, 5) #the centroids are not always the same for year
-#each centroid is a Song object. Each element in assignments is an array of Song objects
-#might need to change this so that trackid is readily accessible
-
-for centroid in centroids: #To see what the years are for the centroids
-    print centroid.year
+# #each centroid is a Song object. Each element in assignments is an array of Song objects
+# #might need to change this so that trackid is readily accessible
+#
+# for centroid in centroids: #To see what the years are for the centroids
+#     print centroid.year
